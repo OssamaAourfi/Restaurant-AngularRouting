06703b5cde3a6e498b59d141f516e2108fbd7b64
OssamaAourfi <aourfidev@gmail.com>
2022-05-24 12:39:05 +0100

    lastReactiveForms

--- src/app/contacts/register/register.component.ts
+++ src/app/contacts/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  Validators } from '@angular/forms';
+import {FormGroup, FormControl  } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,9 +13,22 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  register = new  FormGroup ({
+    name : new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.pattern('[a-zA-Z]+')
+    ]),
+    email : new FormControl('',[
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
+    ]),
+    password : new FormControl('',[
+      Validators.required,
+      Validators.minLength(6)
+    ])
 
-  log(a:any){
-    console.log(a);
-  }
+  })
+ 
 
 }
